79c1e53dbe20a211ee52fe14367ab7c2b6195f51
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-11-18 19:05:56 +0200

    make sub title is nullable at service style save

--- app/Http/Controllers/ServiceControllerResource.php
+++ app/Http/Controllers/ServiceControllerResource.php
@@ -32,11 +32,15 @@ class ServiceControllerResource extends Controller
      */
     public function index()
     {
-
+        return auth()->user()->roleName() == 'client';
         $data = services::query()
             ->when(auth()->user()->roleName() == 'owner',fn($e) => $e->where('user_id',auth()->id()))
-
-           // ->when(auth()->user()->roleName() == 'client',fn($e) => $e->whereHas('private_answers.owner'),fn($q) => $q->where('user_id','=',auth()->id()))
+            ->when(auth()->user()->roleName() == 'member',
+                fn($e) => $e->whereHas('privileges',
+                    fn($q) => $q->where('user_id',auth()->id())
+                              ->whereHas('controls.privilege',fn($x) => $x->where('name','=','view'))
+                ))
+            ->when(auth()->user()->roleName() == 'client',fn($e) => $e->whereHas('private_answers.owner'),fn($q) => $q->where('user_id','=',auth()->id()))
             ->when(auth()->user()->roleName() == 'admin',fn($e) => $e->with('user'))
             ->orderBy('id','DESC');
         $output  = app(Pipeline::class)
